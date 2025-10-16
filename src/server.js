5ffd700ebde7a045b3ec4e2735c806c7dd035203
Omar Angola <omardavidangola@gmail.com>
2025-10-16 09:04:38 -0400

    Agregamos a Cors el origen a nuestro Frontend

--- src/server.js
+++ src/server.js
@@ -1,22 +1,26 @@
-import express from "express"
-import rutasNotas from "./rutas/rutasNotas.js"
-import dontenv from "dotenv"
-import { conectarDB } from "./configuracion/db.js"
-import cors from "cors"
-dontenv.config()
-const app = express()
+import express from "express";
+import rutasNotas from "./rutas/rutasNotas.js";
+import dontenv from "dotenv";
+import { conectarDB } from "./configuracion/db.js";
+import cors from "cors";
+dontenv.config();
+const app = express();
 
-app.use(cors(({
-    origin: "http://localhost:5173"
-})))
-app.use(express.json())
-app.use('/api/notas' , rutasNotas)
+app.use(
+  cors({
+    origin: [
+        "http://localhost:5173", 
+        "https://teal-halva-ea9852.netlify.app/"
+    ],
+  })
+);
+app.use(express.json());
+app.use("/api/notas", rutasNotas);
 
-const PUERTO = process.env.PUERTO || 3001
+const PUERTO = process.env.PUERTO || 3001;
 
-conectarDB()
-.then(() => {
-    app.listen(PUERTO, () => {
-    console.log(`Servidor Levandotado en el Puerto http://localhost:${PUERTO}`)
-})
-})
+conectarDB().then(() => {
+  app.listen(PUERTO, () => {
+    console.log(`Servidor Levandotado en el Puerto http://localhost:${PUERTO}`);
+  });
+});
